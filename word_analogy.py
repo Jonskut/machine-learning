b0d7e33a8eba67090fc32e407f4ccbdd59da7581
Jonskut <jonatantevaniemi@outlook.com>
2024-09-09 12:50:14 +0300

    analogy program finished

--- word_analogy.py
+++ word_analogy.py
@@ -20,14 +20,14 @@ vocab = {w: idx for idx, w in enumerate(words)}
 ivocab = {idx: w for idx, w in enumerate(words)}
 
 # Vocabulary and inverse vocabulary (dict objects)
-print('Vocabulary size')
-print(len(vocab))
-print(vocab['man'])
-print(len(ivocab))
-print(ivocab[10])
+# print('Vocabulary size')
+# print(len(vocab))
+# print(vocab['man'])
+# print(len(ivocab))
+# print(ivocab[10])
 
 # W contains vectors for
-print('Vocabulary word vectors')
+# print('Vocabulary word vectors')
 vector_dim = len(vectors[ivocab[0]])
 W = np.zeros((vocab_size, vector_dim))
 for word, v in vectors.items():
@@ -35,28 +35,55 @@ for word, v in vectors.items():
         continue
     W[vocab[word], :] = v
 
-print(W.shape)
+
+def get_vector(prompt, vocabulary):
+    while True:
+        input_word = input(prompt)
+        if input_word == "EXIT":
+            return None
+        if input_word in vocabulary:
+            return input_word, W[vocabulary[input_word], :]
+        else:
+            print("Word not in vocabulary, try again.")
+
 
 # Main loop for analogy
 while True:
-    input_term = input("\nEnter word (EXIT to break): ")
-    if input_term == 'EXIT':
+    print("Enter 3 words. Enter EXIT to exit")
+    x = get_vector("Enter word X: ", vocab)
+    if x is None:
         break
-    else:
-        similar_words = []
-        input_vector = W[vocab[input_term], :]
-        for word, idx in vocab.items():
-            comparison_vector = W[idx, :]
-            distance = \
-                float(np.sqrt(np.sum((input_vector - comparison_vector) ** 2)))
+    word_x, vector_x = x
 
-            similar_words.append((word, distance))
+    y = get_vector("Enter word Y: ", vocab)
+    if x is None:
+        break
+    word_y, vector_y = y
 
-        # Sort by distance
-        similar_words.sort(key=lambda x: x[1])
+    z = get_vector("Enter word Z: ", vocab)
+    if x is None:
+        break
+    word_z, vector_z = z
 
-        print("\n                               Word       Distance\n")
-        print("---------------------------------------------------------\n")
-        for i in range(3):
-            word, distance = similar_words[i]
-            print("%35s\t\t%f\n" % (word, distance))
+    # Analogy vector
+    input_vector = vector_z + (vector_y - vector_x)
+
+    # Search for nearest neighbor to analogy vector
+    similar_words = []
+    for word, idx in vocab.items():
+        comparison_vector = W[idx, :]
+        distance = \
+            float(np.sqrt(np.sum((input_vector - comparison_vector) ** 2)))
+
+        similar_words.append((word, distance))
+
+    # Sort by distance
+    similar_words.sort(key=lambda x: x[1])
+
+    # Print out
+    print(f"{word_x} is to {word_y} as {word_z} is to")
+    print("\n                               Word       Distance\n")
+    print("---------------------------------------------------------\n")
+    for i in range(2):
+        word, distance = similar_words[i]
+        print("%35s\t\t%f\n" % (word, distance))
