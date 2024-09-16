09d3d22503d11be404621d3109852f35c78a7d10
Jonskut <jonatantevaniemi@outlook.com>
2024-09-16 15:13:57 +0300

    1-NN algorithm complete

--- minst_nn.py
+++ minst_nn.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import random as rand
 import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
 
 
 def class_acc(pred, gt):
@@ -18,37 +19,56 @@ def class_acc(pred, gt):
     return accuracy
 
 
-# Original
-mnist = tf.keras.datasets.mnist
-# New
-# mnist = tf.keras.datasets.fashion_mnist
+def flatten(images):
+    return images.reshape(images.shape[0], 28 * 28)
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# Print the size of training and test data
-print(f'x_train shape {x_train.shape}')
-print(f'y_train shape {y_train.shape}')
-print(f'x_test shape {x_test.shape}')
-print(f'y_test shape {y_test.shape}')
+def train(x_matrix, y_matrix):
+    knn = KNeighborsClassifier(n_neighbors=1)
+    knn.fit(x_matrix, y_matrix)
+    return knn
 
-ground_truth = []
-prediction = []
-iterations = 0
 
-print(range(x_test.shape[0]))
+def show_accuracy(x_test_flat, x_test, y_test, knn):
 
-for i in range(x_test.shape[0]):
-    # Show some images randomly
-    if rand.random() > 0.995:
-        iterations += 1
-        ran = rand.randint(0, 9)
-        plt.figure(1)
-        plt.clf()
-        plt.imshow(x_test[i], cmap='gray_r')
-        plt.title(f"Image {i} label num {y_test[i]} predicted {ran}")
-        ground_truth.append(y_test[i])
-        prediction.append(ran)
-        plt.pause(0.1)
+    predictions = knn.predict(x_test_flat)
 
-acc = class_acc(prediction, ground_truth)
-print(f"accuracy {acc*100}%, {iterations} iterations")
+    for i in range(x_test_flat.shape[0]):
+        # Show some images randomly
+        if rand.random() > 0.999:
+
+            plt.figure(1)
+            plt.clf()
+            plt.imshow(x_test[i], cmap='gray_r')
+            plt.title(f"Image {i} label num "
+                      f"{y_test[i]} predicted {predictions[i]}")
+
+            plt.pause(1)
+
+    acc = class_acc(predictions, y_test)
+    print(f"accuracy {acc*100}%")
+
+
+def main():
+    # Original
+    mnist = tf.keras.datasets.mnist
+    # New
+    # mnist = tf.keras.datasets.fashion_mnist
+
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    # Print the size of training and test data
+    print(f'x_train shape {x_train.shape}')
+    print(f'y_train shape {y_train.shape}')
+    print(f'x_test shape {x_test.shape}')
+    print(f'y_test shape {y_test.shape}')
+
+    x_train = flatten(x_train)
+    x_test_flat = flatten(x_test)
+
+    knn = train(x_train, y_train)
+    show_accuracy(x_test_flat, x_test, y_test, knn)
+
+
+if __name__ == "__main__":
+    main()
