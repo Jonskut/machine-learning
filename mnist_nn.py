b674368d1491b3ee2f836cb182b4787f857cc68d
Jonskut <jonatantevaniemi@outlook.com>
2024-09-16 20:45:38 +0300

    Commented code

--- mnist_nn.py
+++ mnist_nn.py
@@ -7,31 +7,55 @@ import argparse
 
 
 def class_acc(pred, gt):
+    """
+    calculates classifier accuracy
+    :param pred: list, predicted class
+    :param gt: list, actual class
+    :return: float, accuracy from 0-1
+    """
     zeros_amount = 0
-    # calculate difference of vectors
+    # Calculate difference of vectors
     difference = np.subtract(pred, gt)
-    # amount of zeros is amount of correct guesses
+    # Amount of zeros is amount of correct guesses
     for num in difference:
         if num == 0:
             zeros_amount += 1
 
-    # normalize
+    # Normalize
     accuracy = zeros_amount / len(gt)
     return accuracy
 
 
 def flatten(images):
+    """
+    Takes a 28x28 image and shapes it into a 1D vector
+    :param images: array of images
+    :return: list, flattened vector
+    """
     return images.reshape(images.shape[0], 28 * 28)
 
 
-def train(x_matrix, y_matrix):
+def train(x_train, y_train):
+    """
+    trains 1nn-model with scikit learn
+    :param x_train: flattened image vector
+    :param y_train: training labels
+    :return: trained 1nn-model parameters
+    """
     knn = KNeighborsClassifier(n_neighbors=1)
-    knn.fit(x_matrix, y_matrix)
+    knn.fit(x_train, y_train)
     return knn
 
 
 def show_accuracy(x_test_flat, x_test, y_test, knn):
-
+    """
+    shows accuracy and some images to see results of training
+    :param x_test_flat: list, flattened image vector
+    :param x_test: 28x28 image array
+    :param y_test: vector of correct labels
+    :param knn: trained nearest-neighbor model
+    :return: none
+    """
     predictions = knn.predict(x_test_flat)
 
     for i in range(x_test_flat.shape[0]):
@@ -51,6 +75,11 @@ def show_accuracy(x_test_flat, x_test, y_test, knn):
 
 
 def load_dataset(dataset_name):
+    """
+    Loads the dataset from parsed CLI arguments
+    :param dataset_name: str, origin or fashion
+    :return: training and test dataset
+    """
     if dataset_name == 'fashion':
         # Load Fashion MNIST dataset
         (x_train, y_train), (x_test, y_test) = \
